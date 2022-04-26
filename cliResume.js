a5ecff435e04ccf39d3ef047075f7b4cd0e7bcfd
UjjalDeySarkar <ujjaldeysarkar123@gmail.com>
2022-04-27 00:42:06 +0530

    add back and exit

--- cliResume.js
+++ cliResume.js
@@ -1,7 +1,8 @@
 var inquirer = require('inquirer');
 const cp = require('child_process')
 
-inquirer
+function displayDetails(){
+    inquirer
   .prompt([
     {
         type:'list',
@@ -13,15 +14,19 @@ inquirer
     // Use user feedback for... whatever!!
     if(choices.Selection == 'About'){
         console.log('About Ujjal')
+        displayNext()
     }else if(choices.Selection == 'Skills'){
         console.log('C, C#, Java, Python, JS, Node')
+        displayNext()
     }else if(choices.Selection == 'Projects'){
         console.log(`1. Hotel Management in C#,
 2. Payroll System in C#`)
         cp.execSync('open -a "Google Chrome" https://github.com/UjjalDeySarkar') // for mac
+        displayNext()
 
     }else if(choices.Selection == 'Academics'){
         console.log('BCA, MCA')
+        displayNext()
     }
     
   })
@@ -32,3 +37,33 @@ inquirer
       // Something else went wrong
     }
   });
+}
+
+function displayNext(){
+    inquirer
+  .prompt([
+    {
+        type:'list',
+        name: 'goBack',
+        choices: ['Back','Exit']
+    }
+  ])
+  .then((choices) => {
+    // Use user feedback for... whatever!!
+    if(choices.goBack == 'Back'){
+        displayDetails()
+    }else if(choices.goBack == 'Exit'){
+        console.log("Program Exit")
+    }
+    
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
+}
+
+displayDetails()
