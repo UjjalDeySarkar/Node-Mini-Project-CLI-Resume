85e1dbce8eb87a94ac917c832677ee1dc739eb68
UjjalDeySarkar <ujjaldeysarkar123@gmail.com>
2022-04-26 22:18:14 +0530

    add choices (Answers)

--- cliResume.js
+++ cliResume.js
@@ -1,14 +1,29 @@
 var inquirer = require('inquirer');
+const cp = require('child_process')
+
 inquirer
   .prompt([
     {
         type:'list',
-        name: 'Resume',
+        name: 'Selection',
         choices: ['About', 'Skills', 'Projects', 'Academics']
     }
   ])
-  .then((answers) => {
+  .then((choices) => {
     // Use user feedback for... whatever!!
+    if(choices.Selection == 'About'){
+        console.log('About Ujjal')
+    }else if(choices.Selection == 'Skills'){
+        console.log('C, C#, Java, Python, JS, Node')
+    }else if(choices.Selection == 'Projects'){
+        console.log(`1. Hotel Management in C#,
+2. Payroll System in C#`)
+        cp.execSync('open -a "Google Chrome" https://github.com/UjjalDeySarkar') // for mac
+
+    }else if(choices.Selection == 'Academics'){
+        console.log('BCA, MCA')
+    }
+    
   })
   .catch((error) => {
     if (error.isTtyError) {
